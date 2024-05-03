407fc9bcf8f43bf5fd7401c950e1737a978321ba
Chayapan Khannabha <chayapan@gmail.com>
2024-05-03 11:44:00 +0700

    map widget added for UTM conversion func.

--- utm_grid/MapEditor.py
+++ utm_grid/MapEditor.py
@@ -5,6 +5,12 @@
 #
 # https://github.com/TomSchimansky/TkinterMapView
 # https://www.pythontutorial.net/tkinter/tkinter-treeview/
+# https://www.tutorialspoint.com/python/tk_entry.htm
+#
+#  map_widget.fit_bounding_box((<lat1>, <long1>), (<lat2>, <long2>))
+#  marker_1.set_position(48.860381, 2.338594)  # change position
+#  marker_1.delete()
+#  marker_1.set_text("Colosseo in Rome")  # set new text
 #
 # 1. add new marker by clicking right-mouse
 # 2. add polygon
@@ -14,7 +20,6 @@ from tkinter import *
 from tkinter import ttk
 from tkintermapview import TkinterMapView, convert_coordinates_to_address
 import pygeohash as pgh
-
 from utm import utm_zones, from_latlon, UTMZone
 
 class MapEditor(Frame):
@@ -70,38 +75,93 @@ class MapEditor(Frame):
 class UTMCoordinateConverter(Frame):
     def __init__(self, root):
         Frame.__init__(self)
+        # lat,lng
+        self.lat, self.lng = DoubleVar(self), DoubleVar(self)
+        self.zoom, self.utm_coord = IntVar(self), Variable(self)
+        self.geohash_6 = Variable(self)
+
+        # row 1
+
+        # zoom_conf_input = Entry(self, width=4, textvariable=self.zoom, font=('Calibri 10'))
+        zoom_conf_input = Scale(self, label='Zoom:', from_=0, to=20, variable=self.zoom, orient=HORIZONTAL, digits=1, length=100)    
+        zoom_conf_input.grid(row=0,column=0)
+
         label = Label(self, text='UTM')
         label.grid(row=0,column=1)
+
+        utm_coord_input = Entry(self, width=40, textvariable=self.utm_coord, font=('Calibri 16'))
+        utm_coord_input.grid(row=0,column=2)
+
+        convert_btn = Button(self,text='Convert <Enter>', command=lambda : self.convert({}))
+        convert_btn.grid(row=0, column=3)
+
+        # row 2
+
+        lat_scale = Scale(self, label='Latitude:', from_=-90, to=90, variable=self.lat, orient=HORIZONTAL, digits=8, length=210)
+        lat_scale.grid(row=2, column=1)
+        lat_input = Entry(self, textvariable=self.lat, font=('Calibri 10'))
+        lat_input.grid(row=2,column=2)
+
+        # row 3
+
+        server_toggle_btn = Button(self,text='Toggle Tile Server', command=lambda : self.change_tile_server({}))
+        server_toggle_btn.grid(row=3, column=0)
+
+        long_scale = Scale(self, label='Longitude:', from_=-180, to=180, variable=self.lng, orient=HORIZONTAL, digits=9, length=210)
+        long_scale.grid(row=3, column=1)
+        lng_input = Entry(self, textvariable=self.lng, font=('Calibri 10'))
+        lng_input.grid(row=3,column=2)
+
+        # row 4
+        Label(self, text='Geohash', font=('Calibri 16')).grid(row=4,column=0)
+
+        geohash_input = Entry(self, width=12, textvariable=self.geohash_6, font=('Calibri 10'))
+        geohash_input.grid(row=4,column=1)
+
         self.grid_list = Listbox(self)
         for g in utm_zones:
             self.grid_list.insert(0, g)
-        self.grid_list.grid(row=5,column=3)
+        self.grid_list.grid(row=4,column=3)
 
-        # https://www.tutorialspoint.com/python/tk_entry.htm
-        self.lat, self.lng = DoubleVar(self), DoubleVar(self)
-        self.zoom, self.utm_coord = IntVar(self), Variable(self)
-        Entry(self, width=40, textvariable=self.utm_coord, font=('Calibri 16')).grid(row=0,column=2)
-        Entry(self, width=4, textvariable=self.zoom, font=('Calibri 10')).grid(row=2,column=1)
+        # row 5
+        self.map_widget = TkinterMapView(self, width=800, height=300, corner_radius=0)
+        self.map_widget.grid(row=5,column=0,columnspan=4)
+        self.map_widget.set_zoom(15)
+        self.zoom.set(15)
+        self.map_widget.set_position(13.9051778, 100.4880157)  # Bangkok, Thailand
+        self.map_widget.add_right_click_menu_command(label="Add Marker",
+                                        command=self.add_marker, pass_coords=True)
+        self.tile_server = 0 # id of tile server
 
-        lat = Scale(self, label='Latitude:', from_=-90, to=90, variable=self.lat, orient=HORIZONTAL, digits=4, length=210)
-        lat.grid(row=2, column=3)
-        Entry(self, textvariable=self.lat, font=('Calibri 10')).grid(row=3,column=3)
-
-        long = Scale(self, label='Longitude:', from_=-180, to=180, variable=self.lng, orient=HORIZONTAL, digits=6, length=210)
-        long.grid(row=2, column=2)
-        Entry(self, textvariable=self.lng, font=('Calibri 10')).grid(row=3,column=2)
-        Button(self,text='Convert <Enter>', command=lambda : self.convert({})).grid(row=3, column=1)
-
-        self.pack()
+        # self.pack()
         # self.place(relx=0.5, rely=0.5, anchor=CENTER)
         root.utm_widget = self
         root.bind('<Return>',self.convert)
+        self._root = root
 
+    def change_tile_server(self, params=None):
+        """
+        https://github.com/TomSchimansky/TkinterMapView?tab=readme-ov-file#use-other-tile-servers
+        """
+        print("Change tile server.")
+        if self.tile_server == 0:
+            self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)  # google satellite
+            self.tile_server = 1
+        else:
+            self.map_widget.set_tile_server("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png")  # OpenStreetMap (default)
+            self.tile_server = 0
+    
     def convert(self, e):
+        pos = self.map_widget.get_position()
+        self._root.event_list.update("UTM Convert. Map center is %s" % str(pos))
+        self.lat.set(pos[0])
+        self.lng.set(pos[1])
+
+        # 
         lat = float(self.lat.get())
         lng = float(self.lng.get())
         coord = self.utm_coord.get()
-        print(coord)
+        # print(coord)
         output = from_latlon(lat,lng)
         zone = "%s%s" % (output[-2], output[-1])
         z = UTMZone(zone)
@@ -111,7 +171,28 @@ class UTMCoordinateConverter(Frame):
         self.utm_coord.set(output)
         print(e)
         geohash_val = pgh.encode(lat,lng, precision=6)
+        self._root.event_list.update("Geohash: %s" % geohash_val)
+        self.geohash_6.set(geohash_val)
         print(geohash_val)
+
+        gx = int(output[0])
+        gy = int(output[1])
+        txt = f'{zone} / {geohash_val} / {pos}\nx={gx} y={gy}'
+        # self.map_widget.set_marker(self.lat.get(), self.lng.get(), text=txt)
+
+
+    def add_marker(self, coords):
+        print("Add marker:", coords)
+        self.lat.set(coords[0])
+        self.lng.set(coords[1])
+        output = from_latlon(self.lat.get(),self.lng.get())
+        zone = "%s%s" % (output[-2], output[-1])
+        gx = int(output[0])
+        gy = int(output[1])
+        self._root.event_list.update("UTM Add marker %s %s" % (zone, str(coords)))
+        self.map_widget.set_marker(coords[0], coords[1], text=f'{coords[0],coords[1]}\n{zone} x={gx} y={gy}')
+        self.lat.set(coords[0])
+        self.lng.set(coords[1])
 
 class EventList(Frame):
     def __init__(self, root):
@@ -128,13 +209,13 @@ class EventList(Frame):
 def main():
     """Map Editor tool with UTM"""
     root = Tk() # create tkinter window
-    root.geometry(f"{800}x{800}")
+    root.geometry(f"{1200}x{800}+0+0")
     root.title("Map Editor: smoothstreets-core")
     notebook = ttk.Notebook(root)
     notebook.pack(fill=BOTH, pady=10, expand=True)
     # create frames
     frame1 = ttk.Frame(notebook)
-    frame2 = MapEditor(notebook, width=800, height=800)
+    frame2 = MapEditor(notebook, width=1200, height=800)
     frame3 = UTMCoordinateConverter(notebook)
     frame4 = EventList(notebook)
     frame1.pack(fill='both', expand=True)
